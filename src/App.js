e0d256b4d82cd99eafe69ff3fd47b57ecd11cd10
David Nielsen <davidnielsen215@gmail.com>
2019-12-03 14:26:05 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,31 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Best Deal Retailer Login/Signup 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://seetheit.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
+
+        <br></br>
+                    <label className="password">Email</label>
+                    <br></br>
+                    <input className="login-input"
+                        name="username"
+                        type="username"
+                        autoComplete='off'/>
+                        <br></br>
+                    <label className="password">Password</label>
+                    <br></br>
+                    <input className="login-input"
+                        name="password"
+                        type="password"
+                        autoComplete='off'/>
       </header>
     </div>
   );
